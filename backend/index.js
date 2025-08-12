5e023915f9698d1b7fac986eb1ba382cf7bc50a0
captain-adi <maniramp554@gmail.com>
2025-08-12 09:33:46 +0530

    feat : connect to mongoDB atlas

---
+++ backend/index.js
@@ -0,0 +1,5 @@
+import connectDB from "./src/db/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+connectDB();
--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,1 +1,14 @@
-hdskjf
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+export default connectDB;
+
+
