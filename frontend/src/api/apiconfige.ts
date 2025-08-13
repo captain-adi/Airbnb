2dc7e103190647d94c1bf7cdecf37d4b4c30cc8a
captain-adi <maniramp554@gmail.com>
2025-08-13 15:23:48 +0530

    feat : api config

---
+++ frontend/src/api/apiconfige.ts
@@ -0,0 +1,4 @@
+const Api_confige = {
+    API_BASE_URL: "http://localhost:5000/api",
+}
+export default Api_confige;
---
+++ frontend/src/api/apiendpoints.ts
@@ -0,0 +1,24 @@
+import Api_confige from "./apiconfige";
+import axios from "axios";
+class API_ENDPOINTS  {
+    private createURL(endpoint: string){
+        return `${Api_confige.API_BASE_URL}/${endpoint}`;
+    }
+
+    private async fetchData(url: string) {
+        const response = await axios.get(url);
+        if(response.status !== 200){
+            throw new Error("Failed to fetch data");
+        }
+        return response.data;
+    }
+
+    public async getData(endpoint: string) {
+        const url = this.createURL(endpoint);
+        return this.fetchData(url);
+    }
+}
+
+const apiEndpoints = new API_ENDPOINTS();
+
+export default apiEndpoints;
