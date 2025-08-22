2f193de3af425f779c081a2bf3db73040dd25030
captain-adi <maniramp554@gmail.com>
2025-08-22 12:29:00 +0530

    feat : hash password , compare password and generate access token using bcrypt + jwt

--- backend/src/models/user_models.js
+++ backend/src/models/user_models.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
-
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
 const userSchema = new mongoose.Schema({
-  name: {
+  username: {
     type: String,
     required: true,
   },
@@ -16,6 +17,25 @@ const userSchema = new mongoose.Schema({
   },
   
 }, {timestamps : true});
+ 
+userSchema.pre("save" ,  async function (next) {
+ if(!this.isModified("password")) return next() ;
+  this.password = await bcrypt.hash(this.password,10)
+ next();
+})
 
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = async function () {
+return jwt.sign({
+    _id: this._id,
+    email : this.email,
+    username : this.username
+  } , process.env.ACCESS_TOKEN_SCRATE , {
+    expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+  } )
+}
 
 export const User = mongoose.model("User", userSchema);
