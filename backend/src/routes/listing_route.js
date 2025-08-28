4ccef725f8a840dd333ae4aea6b0cc106a788d84
captain-adi <maniramp554@gmail.com>
2025-08-28 09:13:51 +0530

    refactor(listing): follow MVC

--- backend/src/routes/listing_route.js
+++ backend/src/routes/listing_route.js
@@ -5,11 +5,8 @@ import passport from '../middlewares/passport_middleware.js';
 import { isOwner } from '../middlewares/isOwner.js';
 const router = express.Router();
 
-router.route('/').get(showAllListings);
-router.route('/:id').get(showListingById)
-router.route('/').post(validateListing, passport.authenticate('jwt', { session: false }), createListing);
-router.route("/:id").delete(passport.authenticate('jwt', { session: false }),isOwner, deleteListing);
-router.route('/:id').patch(validateListing ,passport.authenticate('jwt', { session: false }),isOwner,updateListing);
+router.route('/').get(showAllListings).post(validateListing, passport.authenticate('jwt', { session: false }), createListing);
+router.route('/:id').get(showListingById).delete(passport.authenticate('jwt', { session: false }),isOwner, deleteListing).patch(validateListing ,passport.authenticate('jwt', { session: false }),isOwner,updateListing);
 
 
 export default router; 
